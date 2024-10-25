5988fe716f0beb95bdac463145ea88777e4f8aee
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-10-24 17:54:48 -0700

    Update dependencies & routes

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
+/**
+ * Imports locales
+ */
 const usuarioRoutes = require("./routes/usuario");
 const sorteoRoutes = require("./routes/sorteo");
+const participanteRoutes = require("./routes/participante");
+const pagoRoutes = require("./routes/pago");
+const numeroRoutes = require("./routes/numero");
+const errorHandler = require("./middlewares/errors");
+const { login, authMiddleware } = require("./middlewares/auth");
 
 const app = express();
+dotenv.config();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://localhost:27017/sorteosdb");
-
 app.use(express.json());
 
+app.post("/login", login);
+app.use(authMiddleware);
+
+/**
+ * Rutas de la API.
+ */
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/sorteos", sorteoRoutes);
+app.use("/api/participantes", participanteRoutes);
+app.use("/api/pagos", pagoRoutes);
+app.use("/api/numeros", numeroRoutes);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Algo salió mal!");
-});
+app.use(errorHandler);
 
 const server = app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
