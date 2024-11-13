49cf28b4ca821a8c4a17208461b11bf81e5a49a0
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-11-13 01:09:28 -0700

    Create API with sorteo creation capabilities

--- frontend/src/components/API.js
+++ frontend/src/components/API.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-export const API = axios.create({
+const API = axios.create({
   baseURL: 'https://proyecto-final-agile.fly.dev/api',
 });
 
 API.createSorteo = async (sorteo) => {
-  return await API.post('/sorteos', sorteo);
+  try {
+    return await API.post('/sorteos', sorteo);
+  } catch (error) {
+    throw error;
+  }
 };
+
+export default API;
