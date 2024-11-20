ae13bc02579d1c4e0f64879a0d7ebcde2bce6dbb
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-11-19 18:20:22 -0700

    Update method to be more descriptive & granular

--- frontend/src/components/SorteoFormValidator.js
+++ frontend/src/components/SorteoFormValidator.js
@@ -37,5 +37,43 @@ export const validateNumbers = (sorteo) => {
   const rangoNumeros = parseInt(sorteo.rangoNumeros, 10);
   const precioNumero = parseFloat(sorteo.precioNumero);
 
-  return rangoNumeros > 0 && precioNumero >= 0;
+  if (isNaN(rangoNumeros) || rangoNumeros <= 0) {
+    return {
+      isValid: false,
+      message: 'El rango de números debe ser un número mayor a 0.',
+    };
+  }
+  if (isNaN(precioNumero) || precioNumero < 0) {
+    return {
+      isValid: false,
+      message: 'El precio por número debe ser un número mayor o igual a 0.',
+    };
+  }
+  return { isValid: true };
+};
+
+export const errorHandler = (error) => {
+  if (error.response) {
+    const errorMessage = error.response.data.error.errorResponse.errmsg;
+
+    if (errorMessage.includes('duplicate')) {
+      return 'El nombre del sorteo ya existe en la base de datos.';
+    }
+
+    if (errorMessage.includes('validation failed')) {
+      return 'Error de validación: por favor verifica los datos ingresados.';
+    }
+
+    if (errorMessage.includes('Cast to ObjectId failed')) {
+      return 'Error de formato: el ID proporcionado no es válido.';
+    }
+
+    if (errorMessage.includes('DocumentNotFoundError')) {
+      return 'Error: el documento solicitado no se encontró en la base de datos.';
+    }
+
+    return errorMessage;
+  }
+
+  return 'Error al crear sorteo';
 };
