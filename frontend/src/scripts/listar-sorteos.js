578f3bf892f85ae47ab7bd881c65911e99b6e546
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-11-28 18:58:04 -0700

    Add missing import

--- frontend/src/scripts/listar-sorteos.js
+++ frontend/src/scripts/listar-sorteos.js
@@ -1,6 +1,7 @@
 import API from '../components/API.js';
 
 import 'material-icons/iconfont/material-icons.css';
+import { decodeInput } from '../components/SorteoFormValidator.js';
 
 const tablaSorteos = document.querySelector('#listaSorteos tbody');
 
