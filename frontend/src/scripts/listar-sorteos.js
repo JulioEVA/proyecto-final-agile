cf1407d246385cd1464f0ecf0d4c2c45f31e9c26
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-11-29 20:54:19 -0700

    mend

--- frontend/src/scripts/listar-sorteos.js
+++ frontend/src/scripts/listar-sorteos.js
@@ -2,6 +2,7 @@ import API from '../components/API.js';
 import Toasted from 'toastedjs/dist/toasted.min.js';
 import 'material-icons/iconfont/material-icons.css';
 import { decodeInput } from '../components/SorteoFormValidator.js';
+import '../components/ConfirmationPopup/ConfirmationPopup.js';
 
 const tablaSorteos = document.querySelector('#listaSorteos tbody');
 
